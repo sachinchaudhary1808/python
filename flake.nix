e7510522391f95e7422b9884fa426406169458d6
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-10-19 19:12:23 +0530

    updated

--- flake.nix
+++ flake.nix
@@ -1,23 +1,25 @@
 {
   description = "A development shell with python-tools using Nix flakes";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
-    devShells = {
-      x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-        buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux = let
+      # Import nixpkgs with permitted insecure packages
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.permittedInsecurePackages = [ "qtwebengine-5.15.19" ];
+      };
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
           python3
           python313Packages.ipython
+          basedpyright
         ];
 
         shellHook = ''
-          echo "Development shell for pythong is Activated"
+          echo "Development shell for python is Activated"
         '';
       };
     };
